33d5c094381d7ee60e71df9d3bca71987285602b
notlega <onghinjourn@gmail.com>
2023-04-20 17:40:26 +0800

    feat: :sparkles: initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# share-function
+
+Pet project on hash functions to shorten urls
+
+## Installation
+
+```bash
+git clone https://github.com/deadgravy/share-function.git
+```
+
+## Usage
+
+Install pnpm
+
+```bash
+npm i -g pnpm
+```
+
+Install dependencies
+
+```bash
+pnpm i --frozen-lockfile
+```
+
+Setup SQLite and Prisma
+
+```bash
+pnpm run prisma
+```
+
+Run the server
+
+```bash
+pnpm run dev
+```
---
+++ prisma/seed.ts
@@ -0,0 +1,65 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaClient: PrismaClient = new PrismaClient();
+
+const main = async () => {
+  console.log("Clearing database...");
+
+  await Promise.allSettled([
+    prismaClient.listing_images.deleteMany({}),
+    prismaClient.listings.deleteMany({}),
+  ]);
+
+  console.log("Database cleared");
+  console.log("Seeding public.listings...");
+
+  const listing1 = await prismaClient.listings.create({
+    data: {
+      name: "Selling metal beam",
+      price: 100.01,
+      description: "This is a metal beam",
+    },
+  });
+
+  console.log(listing1);
+
+  const listing2 = await prismaClient.listings.create({
+    data: {
+      name: "Selling metal rod",
+      price: 200.02,
+    },
+  });
+
+  console.log(listing2);
+  console.log("Successfully seeded public.listings")
+  console.log("Seeding public.listing_images...");
+
+  const listingImage1 = await prismaClient.listing_images.create({
+    data: {
+      listing_id: listing1.id,
+      image: "https://picsum.photos/1920/1080",
+    },
+  });
+
+  console.log(listingImage1);
+
+  const listingImage2 = await prismaClient.listing_images.create({
+    data: {
+      listing_id: listing2.id,
+      image: "https://picsum.photos/1920/1080",
+    },
+  });
+
+  console.log(listingImage2);
+  console.log("Successfully seeded public.listing_images");
+};
+
+main()
+  .then(async () => {
+    await prismaClient.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prismaClient.$disconnect();
+    process.exit(1);
+  });
