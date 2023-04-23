1bda1b03182507b803e3a2bd87503ffc811def51
Wu Yining <yyiningsp@gmail.com>
2023-04-24 00:03:53 +0800

    feat: Created a [id] listing page
    Created a [id] listing page for each listing with styled div component

---
+++ src/components/modal.tsx
@@ -0,0 +1,52 @@
+import { Button, Modal, Box } from '@mui/material';
+import { useState } from 'react';
+
+const data = {
+  link: 'https://www.google.com',
+};
+
+const ModalPopUp = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <div>
+      <Button
+        variant="outlined"
+        onClick={handleOpen}
+        sx={{ display: 'flex', justifyContent: 'left', ml:20 }}
+      >
+        Share
+      </Button>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="child-modal-title"
+        aria-describedby="child-modal-description"
+      >
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            border: '2px solid #000',
+            boxShadow: 24,
+            width: 300,
+            padding: 2,
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+          }}
+        >
+          <h2 id="child-modal-title">Copy this link</h2>
+          <p id="child-modal-description">{data.link}</p>
+          <Button variant="contained" onClick={handleClose}>
+            Close
+          </Button>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalPopUp;
---
+++ src/pages/specificListing/[id].tsx
@@ -0,0 +1,65 @@
+import ModalPopUp from '@/components/modal';
+import DivComponent from '@/components/styledDiv';
+import { Listing } from '@/types/listing';
+import { Box, Card, Typography, Button } from '@mui/material';
+import { useRouter } from 'next/router';
+
+const data : Listing = 
+  {
+    title: 'Listing 1',
+    description: 'Metal bar description 1... goes till very long',
+    price: 400,
+  }
+
+const SpecificListing = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  console.log(id);
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <div>
+        <Card sx={{ borderRadius: 5, width: 1000 }}>
+          <DivComponent>
+            <Typography
+              variant="h5"
+              sx={{ fontWeight: 'medium', textAlign: 'left', ml: 20, mb: 2 }}
+            >
+              {data.title}
+            </Typography>
+            <Typography
+              variant="h5"
+              sx={{ fontWeight: 'bold', textAlign: 'left', ml: 20, mb: 2 }}
+            >
+              S${data.price}
+            </Typography>
+            <Typography
+              variant="h5"
+              sx={{ fontWeight: 'medium', textAlign: 'left', ml: 20, mb: 2 }}
+            >
+              Description
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{ fontWeight: 'regular', textAlign: 'left', mx: 20, mb: 2 }}
+            >
+              {data.description}
+            </Typography>
+            {/* <Button sx={{display: 'flex', justifyContent:'left', ml: 19}}>
+              Share
+            </Button> */}
+            <ModalPopUp/>
+          </DivComponent>
+        </Card>
+      </div>
+    </Box>
+  );
+};
+
+export default SpecificListing;
