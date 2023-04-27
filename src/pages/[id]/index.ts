a4502b768e902df79c4b45451d03766eab95ab45
Ong Hin Journ <onghinjourn@gmail.com>
2023-04-28 06:04:13 +0800

    fix: :bug: fixes typeerrors and hashing

--- src/pages/[id]/index.ts
+++ src/pages/[id]/index.ts
@@ -1,41 +1,32 @@
-import { GetServerSideProps } from 'next';
-import { PrismaClient } from '@prisma/client';
+import type { GetServerSideProps } from 'next';
+import prisma from '@/utils/prisma';
 
-const prisma = new PrismaClient();
+const RedirectPage = (): null => null;
 
-const ExamplePage = () => null;
+const getServerSideProps: GetServerSideProps = async (context) => {
+  const { req } = context;
+  const inputUrl = req.url;
+  const searchUrl = inputUrl?.substring(1);
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-    const { req, res } = context;
-    const inputUrl = req.url;
-    const shortUrl = `${process.env.FRONTEND_URL}${inputUrl as string}`;
-    const searchUrl = inputUrl?.substring(1)
+  const listing = await prisma.listings.findUnique({
+    where: {
+      hashed_url: searchUrl as string,
+    },
+  });
 
+  if (listing === null) {
+    return {
+      notFound: true,
+    };
+  }
 
-    // Query the database for a listing with a matching hashed_url 
-    console.log(searchUrl)
-    const listing = await prisma.listings.findUnique({
-        where: {
-            hashed_url: searchUrl as string,
-        },
-    });
-
-    if (listing) {
-        console.log(listing);
-        // Redirect to localhost:3000/listing/{product_name}-{id}
-        var product_name = listing.name
-        var id = listing.id
-        res.setHeader('Location', 'http://localhost:3000/listing/' + product_name + '-' + id);
-        res.statusCode = 302;
-        res.end();
-    } else {
-        console.log(`Listing not found for URL: ${shortUrl}`);
-        res.statusCode = 404;
-        res.end();
-    }
-
-    // Return an object with inputUrl
-    return { props: { inputUrl } };
+  return {
+    props: {},
+    redirect: {
+      destination: `http://localhost:3000/listing/${listing.name}-${listing.id}`,
+    },
+  };
 };
 
-export default ExamplePage;
+export { getServerSideProps };
+export default RedirectPage;
--- src/pages/api/[id]/index.ts
+++ src/pages/api/[id]/index.ts
@@ -1,53 +1,51 @@
-import { PrismaClient } from '@prisma/client';
+import prisma from '@/utils/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { nanoid } from 'nanoid'; // Import nanoid library for generating short IDs
-
-const prisma = new PrismaClient(); // Create a Prisma Client instance
+import { shortenUrl } from '@/utils/shortenUrl';
 
 const handler = async (
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ): Promise<void> => {
-    const {
-        query: { id },
-    } = req; // Retrieve the listing ID from the URL query parameter
+  const { id } = req.query; // Retrieve the listing ID from the URL query parameter
 
-    try {
-        // Use Prisma Client to query the "listings" model and retrieve the listing
-        const listing = await prisma.listings.findUnique({
-            where: { id: parseInt(id as string) }, // Convert the listing ID to an integer
-        });
+  try {
+    // Use Prisma Client to query the "listings" model and retrieve the listing
+    const listing = await prisma.listings.findUnique({
+      where: { id: parseInt(id as string, 10) }, // Convert the listing ID to an integer
+    });
 
-        if (listing !== null) {
-            // If the listing is found, check if hashed URL is already present
-            if (listing.hashed_url == null) {
-                // If hashed URL is not present, generate a shortened URL using nanoid
-                const hashedUrl = nanoid(10); // Generate a 10-character long random ID
-                // Use Prisma Client to update the listing with the shortened URL
-                await prisma.listings.update({
-                    where: { id: listing.id },
-                    data: { hashed_url: hashedUrl },
-                });
-                // Construct the complete URL with the hashed URL appended to the base URL
-                const shortUrl = `${process.env.FRONTEND_URL}/${hashedUrl as string}`;
-                // Return the shortened URL value in the response
-                res.status(200).json({ hashedUrl: shortUrl });
-            } else {
-                // If hashed URL is already present, construct the complete URL with the hashed URL appended to the base URL
-                const shortUrl = `${process.env.FRONTEND_URL}/${listing.hashed_url}`;
-                // Return the shortened URL value in the response
-                res.status(200).json({ hashedUrl: shortUrl });
-            }
-        } else {
-            // If the listing is not found, return an error message in the response
-            res.status(404).json({ error: 'Listing not found' });
-        }
-    } catch (error) {
-        // If there's an error, return an error message in the response
-        res.status(500).json({ error: 'Internal server error' });
-    } finally {
-        await prisma.$disconnect(); // Disconnect Prisma Client after using it
+    if (listing === null) {
+      res.status(404).json({ error: 'Listing not found' });
+      return;
     }
+
+    // If the listing is found, check if hashed URL is already present
+    if (listing.hashed_url === null) {
+      // If hashed URL is not present, generate a shortened URL using nanoid
+      const hashedUrl = shortenUrl(); // Generate a 10-character long random ID
+
+      // Use Prisma Client to update the listing with the shortened URL
+      await prisma.listings.update({
+        where: { id: listing.id },
+        data: { hashed_url: hashedUrl },
+      });
+
+      // Construct the complete URL with the hashed URL appended to the base URL
+      const shortUrl = `${process.env.FRONTEND_URL}/${hashedUrl}`;
+      // Return the shortened URL value in the response
+      res.status(200).json({ data: { shortUrl } });
+      return;
+    }
+
+    // If hashed URL is already present, construct the complete URL with the hashed URL appended to the base URL
+    const shortUrl = `${process.env.FRONTEND_URL}/${listing.hashed_url}`;
+    // Return the shortened URL value in the response
+    res.status(200).json({ data: { shortUrl } });
+    return;
+  } catch (error) {
+    // If there's an error, return an error message in the response
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
 
 export default handler; // Export the handler function
