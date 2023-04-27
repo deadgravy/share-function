dea3c6cab60894f988cbd34c6e55c6973e96e50f
Adeeb <adityaadeeb04@gmail.com>
2023-04-27 19:17:57 +0800

    fully intercepted

--- src/pages/[id]/index.ts
+++ src/pages/[id]/index.ts
@@ -9,19 +9,23 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     const { req, res } = context;
     const inputUrl = req.url;
     const shortUrl = `${process.env.FRONTEND_URL}${inputUrl as string}`;
+    const searchUrl = inputUrl?.substring(1)
 
-    // Query the database for a listing with a matching hashed_url
+
+    // Query the database for a listing with a matching hashed_url 
+    console.log(searchUrl)
     const listing = await prisma.listings.findUnique({
         where: {
-            hashed_url: shortUrl,
+            hashed_url: searchUrl as string,
         },
     });
 
     if (listing) {
         console.log(listing);
-
-        // Redirect to google.com
-        res.setHeader('Location', 'https://www.google.com');
+        // Redirect to localhost:3000/listing/{product_name}-{id}
+        var product_name = listing.name
+        var id = listing.id
+        res.setHeader('Location', 'http://localhost:3000/listing/' + product_name + '-' + id);
         res.statusCode = 302;
         res.end();
     } else {
